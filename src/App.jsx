b51574163ba0b21981970f6455c58c0f8e47d514
Jose Lopez Renteria <joselukas24@gmail.com>
2023-08-13 21:21:58 -0500

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import "./App.css";
+import MainHeader from "./components/MainHeader";
+import Product from "./components/Product";
+import MainFooter from "./components/MainFooter";
+
+function App() {
+  return (
+    <div>
+      <MainHeader />
+      <Product />
+      <MainFooter />
+    </div>
+  );
+}
+
+export default App;
