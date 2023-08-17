59487e7c065be053741beb2c95b229f6483884ac
Jose Lopez Renteria <joselukas24@gmail.com>
2023-08-17 14:11:16 -0500

    Reset

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,7 @@
 import "./App.css";
-import MainHeader from "./components/MainHeader";
-import Product from "./components/Product";
-import MainFooter from "./components/MainFooter";
 
 function App() {
-  return (
-    <div>
-      <MainHeader />
-      <Product />
-      <MainFooter />
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
