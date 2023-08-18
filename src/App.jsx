903b4cc3055591478ea568b4f6397b6b1dc89162
Jose Lopez Renteria <joselukas24@gmail.com>
2023-08-18 12:58:33 -0500

    created the main page component

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,10 @@
 import "./App.css";
-import StoreNavigation from "./components/StoreNavigation";
-import FeatureSection from "./components/FeatureSection";
-import NintendoProductFeatures from "./components/NintendoProductFeatures";
-import PlaystationProductFeatures from "./components/PlaystationProductFeatures";
-import XboxProductFeatures from "./components/XboxProductFeatures";
-import HeaderSection from "./components/HeaderSection";
-import Logos from "./components/Logos";
-import Footer from "./components/Footer";
+import MainPage from "../src/pages/MainPage";
 
 function App() {
   return (
     <div>
-      <StoreNavigation />
-      <HeaderSection />
-      <FeatureSection />
-      <NintendoProductFeatures />
-      <Logos />
-      <PlaystationProductFeatures />
-      <XboxProductFeatures />
-      <Footer />
+      <MainPage />
     </div>
   );
 }
