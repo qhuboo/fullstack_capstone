e8eeffec9d234c3d20f09fe6884ffa62e9803a50
Jose Lopez Renteria <joselukas24@gmail.com>
2023-08-18 21:28:58 -0500

    added pagination

--- src/pages/MarketPlace.jsx
+++ src/pages/MarketPlace.jsx
@@ -1,12 +1,14 @@
 import Footer from "../components/Footer";
 import StoreNavigation from "../components/StoreNavigation";
 import ProductList from "../components/ProductList";
+import Pagination from "../components/Pagination";
 
 export default function MarketPlace() {
   return (
     <div>
       <StoreNavigation />
       <ProductList />
+      <Pagination />
       <Footer />
     </div>
   );
