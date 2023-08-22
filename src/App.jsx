dece8312733805db67b9ebfcf15b9ab125260e59
Natnael Gebremeden <Natnaelatnafu25@gmail.com>
2023-08-21 19:59:40 -0500

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,19 @@
 import "./App.css";
+import Sign_IN from "./Components/Sign_In";
+import Shopping_Cart from "./Components/Shopping_Cart";
+import CheckoutPage from "./Components/CheckoutPage";
 
 function App() {
-  return <div></div>;
+  return (
+    // <Shopping_Cart />
+    <CheckoutPage/>
+    // <Router>
+    //   <Switch>
+    //     <Route path="/checkout" component={CheckoutPage} />
+    //     <Route path="/" component={Shopping_Cart} />
+    //   </Switch>
+    // </Router>
+  );
 }
 
 export default App;
