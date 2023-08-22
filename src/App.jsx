bad057fd0be3374bc6b5ddfe12cdb2d5a4bf1c6a
Jose Lopez Renteria <joselukas24@gmail.com>
2023-08-21 21:25:06 -0500

    successfully changed shopping cart

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,19 @@ import "./App.css";
 import MainPage from "./pages/MainPage";
 import MarketPlace from "./pages/MarketPlace";
 import ProductPage from "./pages/ProductPage";
+import Shopping_Cart from "./pages/Shopping_Cart";
 import CheckoutPage from "./pages/CheckoutPage";
 import Sign_In from "./pages/Sign_In";
 
 function App() {
   return (
     <div>
-      <MainPage />
+      {/* <MainPage />
       <MarketPlace />
-      <ProductPage />
-      <CheckoutPage />
-      <Sign_In />
+      <ProductPage /> */}
+      <Shopping_Cart />
+      {/* <CheckoutPage />
+      <Sign_In /> */}
     </div>
   );
 }
