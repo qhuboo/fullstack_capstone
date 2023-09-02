029ef809a9dffa82c3fa7f734f0f2fbb8aa799d0
Jose Lopez Renteria <joselukas24@gmail.com>
2023-09-02 00:09:24 -0500

    removed link import

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { Routes, Route, Link } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import "./App.css";
 import MainPage from "./pages/MainPage";
 import MarketPlace from "./pages/MarketPlace";
