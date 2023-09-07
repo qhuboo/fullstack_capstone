086d05b28fd8b7eb1f71a9109639c328d614ab38
Jose Lopez Renteria <joselukas24@gmail.com>
2023-09-07 17:27:47 -0500

    connected to the backend and fetched all game data

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ import game_data from "./data/game_data";
 function App() {
   useEffect(() => {
     const fetchData = async () => {
-      const response = await fetch("http://localhost:3000");
+      const response = await fetch("http://localhost:3000/api/games/game/141");
       const data = await response.json();
       console.log(data);
     };
